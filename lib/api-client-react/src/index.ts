f494ed5df4a77cf87dc474ef55930255ca07be50
Mark Lawson <mark.lawson@cleveranchor.com>
2026-06-30 11:50:34 -0400

    updated deploy-api.yml to use branch main

--- lib/api-client-react/src/index.ts
+++ lib/api-client-react/src/index.ts
@@ -2,3 +2,4 @@ export * from "./generated/api";
 export * from "./generated/api.schemas";
 export * from "./learner-details";
 export { setBaseUrl, setAuthTokenGetter } from "./custom-fetch";
+export type { AuthTokenGetter } from "./custom-fetch";
